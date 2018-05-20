969c93690b2e85b5a9c5f438689c26b9dccf51d5
magdy <magdyksaleh@gmail.com>
2018-05-20 16:31:57 -0700

    updated dataset class

--- Lung_dataset.py
+++ Lung_dataset.py
@@ -16,7 +16,7 @@ warnings.filterwarnings("ignore")
 
 #Create dataset class
 class ILDDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, train=False):
+    def __init__(self, csv_file, root_dir, mask=False,  transform=None, train=False):
         
         #args: csv_file path and filename of file
         #      root_Dir dir to dataset
@@ -25,6 +25,7 @@ class ILDDataset(Dataset):
         self.root_dir = root_dir
         self.transform = transform
         self.train = train
+        self.mask = mask
     
     def __len__(self):
         return len(self.slice_labels)
@@ -44,20 +45,36 @@ class ILDDataset(Dataset):
                     continue
                 slice_path = os.path.join(scan_path,list_of_slices[slice_num])
                 if (cntr == idx):
-                    return slice_path, int(list_of_scans[scan_num])
+                    return slice_path, int(list_of_scans[scan_num]), slice_num, scan_path, list_of_slices[slice_num]
                 cntr += 1
+    
+    def find_mask_path(self, scan_path, slice_name):
+        mask_path = os.path.join(scan_path,"lung_mask")
+        slice_num = int(slice_name[-6:-4])
+        for mask in os.listdir(mask_path):
+            if (mask[-4:] != ".dcm"):
+                continue
+            if(mask[-6:-4].isdigit()):
+                if(int(mask[-6:-4]) == slice_num):
+                    return os.path.join(mask_path, mask)
+            elif((slice_num<10) and (mask[-5:-4].isdigit()) and (int(mask[-5:-4]) == slice_num)):
+                return os.path.join(mask_path, mask)
          
     def __getitem__(self, idx):
-        slice_path, scan_num = self.find_slice_path(idx)
+        slice_path, scan_num, slice_num, scan_path, slice_name = self.find_slice_path(idx)
+        mask_path = self.find_mask_path(scan_path, slice_name)
         ds=pydicom.read_file(slice_path)
         hu_img = ds.RescaleIntercept + ds.pixel_array*ds.RescaleSlope
-        # if(hu_img.shape != (512,512)):
-        hu_img = transform.resize(hu_img, (64, 64), mode='constant')
+        if ((mask_path is not None )and (self.mask == True)):
+            mask=pydicom.read_file(mask_path).pixel_array
+        else:
+            mask=np.ones_like(hu_img)
+        filtered_im = np.asarray(hu_img)*np.asarray(mask)
+        filtered_im = transform.resize(filtered_im, (64, 64), mode='constant')
 
         #grab label
         label = self.slice_labels[np.where(self.slice_labels[:,0] == scan_num)][0][1]
-        
-        sample = (np.asarray(hu_img), label)
+        sample = (filtered_im, label)
         if self.transform:
             sample = self.transform(sample)
         return sample
